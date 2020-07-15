fd1bf666e551ddd1e452bb0028a51bb7424f8ae7
vagrant <vagrant@localhost.localdomain>
2020-07-15 05:07:05 +0000

    [Add]マイページ

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -2,17 +2,27 @@ class Public::CustomersController < ApplicationController
   def top
   end
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to public_customer_path
   end
 
   def unsubscribe
   end
 
   def withdraw
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_katakana, :last_name_katakana, :email, :phone_number, :address, :postal_code)
   end
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,6 +1,29 @@
 class Public::OrdersController < ApplicationController
   def comfirm
-    byebug
+    @cart_products = current_customer.cart_products
+    @price_on_purchase = 0
+    @cart_products.each do |cart_product|
+      @price_on_purchase += cart_product.product.non_taxed_price * 1.1.to_i * cart_product.count
+    end
+
+    if params[:order][:method_of_payment] == "クレジットカード"
+      @method_of_payment = "クレジットカード"
+    else
+      @method_of_payment = "銀行振込"
+    end
+
+    if params[:order][:select_address] == "ご自身の住所"
+      @postal_code = current_customer.postal_code
+      @address = current_customer.address
+      @first_name = current_customer.first_name
+      @last_name = current_customer.last_name
+    elsif params[:order][:select_address] == "登録済住所から選択"
+      @select_address = ShippingAddress.find(params[:id])
+      @address = @select_address.address_info
+    elsif params[:order][:select_address] == "新しいお届け先"
+      @select_address = ShippingAddress.find(params[:id])
+    end
+
   end
 
   def new
