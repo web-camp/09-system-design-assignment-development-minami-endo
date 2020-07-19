a1c4f8e2684d5247a69552b8e34f52be59c5e8a6
vagrant <vagrant@localhost.localdomain>
2020-07-19 07:20:31 +0000

    [Fix]navbar

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -15,7 +15,7 @@ class Public::CustomersController < ApplicationController
   def update
     @customer = current_customer
     @customer.update(customer_params)
-    redirect_to public_customer_path
+    redirect_to public_customers_my_page_path
   end
 
   def unsubscribe
