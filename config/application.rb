01849aa5a46422091e5093f058d42ec5aed82879
vagrant <vagrant@localhost.localdomain>
2020-08-09 11:20:28 +0000

    [Fix]

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module DwcRailsAppTemplate
   class Application < Rails::Application
+  	Refile.secret_key = '1a3a6546db4911200f0a63c2cce77fd0ad96d3fd1ac044b9ed7b0c8b31a571906b61c62452221d8b7512e7adb23e7dfcfec904fadcb6e75e2a70e5eb955b5722'
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
